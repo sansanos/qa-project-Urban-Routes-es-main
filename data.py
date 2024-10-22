90822bef7dc6bb53d21461e7931cc795da25d772
sansanos <san-san-05@hotmail.com>
2024-10-22 16:48:08 -0500

    Commit 4

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-a59bf02b-3dce-4923-9eff-c5142fe357b8.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-2695c36b-a569-4f8c-89df-4e2043aa0d22.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
+from data import message_for_driver
+
 
 # no modificar
 def retrieve_phone_code(driver) -> str:
@@ -35,22 +37,23 @@ def retrieve_phone_code(driver) -> str:
         return code
 
 
-#Clase para agregar direccion desde y hasta
+# Clase para agregar direccion desde y hasta
 class UrbanRoutesPage:
-    #Indicar Marcadores
+
+    # Localizadores
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
 
-    #Generar constructor
+    # Constructor
     def __init__(self, driver):
         self.driver = driver
 
-    #Generar Accion
-    def set_from(self, from_address):
-        self.driver.find_element(*self.from_field).send_keys(from_address)
+    # Acciones
+    def set_from(self, address_from):
+        self.driver.find_element(*self.from_field).send_keys(address_from)
 
-    def set_to(self, to_address):
-        self.driver.find_element(*self.to_field).send_keys(to_address)
+    def set_to(self, address_to):
+        self.driver.find_element(*self.to_field).send_keys(address_to)
 
     def return_from(self):
         return self.driver.find_element(*self.from_field).get_property('value')
@@ -58,60 +61,74 @@ class UrbanRoutesPage:
     def return_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
-    def set_rout(self,from_address,to_address):
-        self.set_from(from_address)
-        self.set_to(to_address)
+    def set_rout(self, address_from, address_to):
+        self.set_from(address_from)
+        self.set_to(address_to)
 
-#Seleccionar modo de viaje
+# Seleccion del modo de viaje
 class ComfortMethod:
-    comfort_button=(By.XPATH,".//*[text()='Comfort']" )
-    def __init__(self,driver):
+
+    button_comfort = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[1]/div[5]/div[2]') # click()
+
+    def __init__(self, driver):
         self.driver=driver
+
     def select_comfort(self):
-        self.driver.find_element(*self.comfort_button).click()
+        self.driver.find_element(*self.button_comfort).click()
+
     def return_status_trip(self):
-        return self.driver.find_element(*self.comfort_button).is_displayed()
+        return self.driver.find_element(*self.button_comfort).is_displayed()
 
-#Clase para seccion de llenado de numero telefonico
+# Clase para rellenar el numero telefonico
 class AddPhoneNumber:
-    place_to_phone_nomber=(By.ID,"phone")
-    next_button=(By.XPATH,".//div[@class='buttons']/button[@class='button full']")
 
-    def __init__(self,driver):
+    phone_number_holder = (By.ID, 'phone') # send_keys()
+    button_phone_next = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[2]/button') # click()
+
+    def __init__(self, driver):
         self.driver=driver
 
-    def whrite_phon_number(self,number):
-        self.driver.find_element(*self.place_to_phone_nomber).send_keys(number)
+    def set_phon_number(self,number):
+        self.driver.find_element(*self.phone_number_holder).send_keys(data.phone_number)
 
-    def post_number(self):
-        self.driver.find_element(*self.next_button).click()
+    def next_button_phone(self):
+        self.driver.find_element(*self.button_phone_next).click()
 
     def return_phon_number(self):
-        return self.driver.find_element(*self.place_to_phone_nomber).get_property('value')
-    def sent_phone_nomber(self,number):
-        self.whrite_phon_number(number)
-        self.post_number()
+        return self.driver.find_element(*self.phone_number_holder).get_property('value')
+
+    def sent_phone_nomber(self,phone_number):
+        self.set_phon_number(phone_number)
+        self.next_button_phone()
 
 
-#Clase para colocar un nuevo mensaje para el conductor
-class SendNewMessage:
-    coment_space=(By.CSS_SELECTOR,"#comment")
+# Mensaje para el conductor
+class SendMessage:
+
+    message_for_driver_holder = (By.CSS_SELECTOR, '#comment') # send_keys()
+
     def __init__(self,driver):
         self.driver=driver
-    def white_mew_message(self,message):
-        self.driver.find_element(*self.coment_space).send_keys(message)
+
+    def set_message(self,message_for_driver):
+        self.driver.find_element(*self.message_for_driver_holder).send_keys(message_for_driver)
+
     def return_message(self):
-        return self.driver.find_element(*self.coment_space).get_property('value')
+        return self.driver.find_element(*self.message_for_driver_holder).get_property('value')
 
-#Clase para validar solicitud de manta y pañuelo
-class AskBlanket:
-    ask_blanket_button=(By.XPATH,"//div[@class='r-sw-container']/div[@class='r-sw']/div[@class='switch']")
+#Clase para seleccionar manta y pañuelo
+class BlanketHankySlider:
+
+    blanket_hanky_slider = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span') # click()
+
     def __init__(self,driver):
         self.driver=driver
+
     def ask_blanket(self):
-        self.driver.find_element(*self.ask_blanket_button).click()
-    def return_estatus_blanket(self):
-        return self.driver.find_element(*self.ask_blanket_button).is_displayed()
+        self.driver.find_element(*self.blanket_hanky_slider).click()
+
+    def return_displayed_blanket(self):
+        return self.driver.find_element(*self.blanket_hanky_slider).is_displayed()
 
 class TestUrbanRoutes:
     driver=None
@@ -124,33 +141,33 @@ class TestUrbanRoutes:
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
         cls.driver = webdriver.Chrome()
 
-    #Prueba para validar el alta de datos origen y destino
+    # 1 - Configurar la direccion
     def test_set_a_route(self):
         self.driver.get(data.urban_routes_url)
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.ID,"from")))
-        route_page=UrbanRoutesPage(self.driver)
+        route_page = UrbanRoutesPage(self.driver)
         addres_from = data.address_from
-        addres_to= data.address_to
-        route_page.set_rout(addres_from,addres_to)
-        assert route_page.return_from()==addres_from
-        assert route_page.return_to()==addres_to
+        addres_to = data.address_to
+        route_page.set_rout(addres_from, addres_to)
+        assert route_page.return_from() == addres_from
+        assert route_page.return_to() == addres_to
 
-    #Prueba 2 Validar la seleccion de metodo de traslado comfort
+    # 2 - Seleccionar la tarifa Comfort
     def test_select_comfort(self):
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,".//div[@class='results-text']/button[@class='button round']")))
         self.driver.find_element(By.XPATH,".//div[@class='results-text']/button[@class='button round']").click()
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,".//*[text()='Comfort']")))
-        travel_method=ComfortMethod(self.driver)
+        travel_method = ComfortMethod(self.driver)
         travel_method.select_comfort()
         WebDriverWait(self.driver,10)
-        assert travel_method.return_status_trip()==True
+        assert travel_method.return_status_trip() == True
 
-    #Prueba 3 Comprobar el llenado de numero telefonico
+    # 3 - Rellenar el número de teléfono
     def test_select_phone_number(self):
         self.driver.find_element(By.CLASS_NAME,"np-text").click()
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.ID,"phone")))
-        send_new_phone=AddPhoneNumber(self.driver)
-        new_phone=data.phone_number
+        send_new_phone = AddPhoneNumber(self.driver)
+        new_phone = data.phone_number
         send_new_phone.sent_phone_nomber(new_phone)
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.ID,"code")))
         WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.ID, "code")))
@@ -158,46 +175,49 @@ class TestUrbanRoutes:
         self.driver.find_element(By.XPATH, ".//*[text()='Confirmar']").click()
         assert send_new_phone.return_phon_number() == new_phone
 
-    def test_add_credit_car_part_1(self):
+    # 4 - Agregar una tarjeta de crédito
+    def test_add_car(self):
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,".//div[@class='pp-button filled']")))
         self.driver.find_element(By.XPATH,".//div[@class='pp-button filled']").click()
         WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH,".//img[@src='/static/media/card.411e0152.svg']")))
         self.driver.find_element(By.XPATH,".//img[@src='/static/media/card.411e0152.svg']").click()
         WebDriverWait(self.driver,10)
-        number_card=data.card_number
-        number_cvv=data.card_code
+        number_card = data.card_number
+        number_cvv = data.card_code
         self.driver.find_element(By.ID,"number").send_keys(number_card,Keys.TAB,number_cvv)
         return self.driver.find_element(By.ID,"number").get_property('value')
-        assert self.driver.find_element(By.ID,"number")==number_card
+        assert self.driver.find_element(By.ID,"number") == number_card
 
     def test_quit_card_modal(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/button').click()
+        return self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/button').click() # click()
 
-    #Prueba 5 escribir un mensaje para el controlador
-    def test_whrite_message(self):
-        WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable((By.ID,"comment")))
-        message=data.message_for_driver
-        whrite_message=SendNewMessage(self.driver)
-        whrite_message.white_mew_message(message)
-        assert whrite_message.return_message()==message
-        time.sleep(4)
+    # 5 - Escribir un mensaje para el conductor
+    def test_set_message(self):
+        WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.ID, "comment")))
+        message = data.message_for_driver
+        set_message = SendMessage(self.driver)
+        set_message.set_message(message)
+        assert set_message.return_message() == message
 
-
-    #Prueba 6 Pedir una manta y pañuelo
-    def test_ask_for_blanket(self):
+    # 6 - Pedir una manta y pañuelos
+    def test_slider_blanket_hanky(self):
         return self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span').click()
 
+    # 7 - Pedir 2 helados
+    def test_1_ice_cream(self):
+        return self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]').click()
+        assert self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[2]') == '1'
 
-    #Prueba 7 solicitar 2 helados
-    def test_2_icecream(self):
-        time.sleep(3)
-        self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]').click()
-        self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]').click()
+    def test_2_ice_cream(self):
+        return self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]').click()
         assert self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[2]') == '2'
 
-    #Prueba 8
-    def test_final_button(self):
+    # 8 - Aparece el modal para buscar un taxi
+    def test_pedir_taxi(self):
+        time.sleep(4)
+        return self.driver.find_element(By.CLASS_NAME, 'smart-button-main').click()
         assert self.driver.find_element(By.CSS_SELECTOR,"smart-button-secondary").is_enabled() == True
+
 
     @classmethod
     def teardown_class(cls):
